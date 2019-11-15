3b6e326f6a52d9a66429c4c3d1f02e3ad6e430f4
Jan <jan.bogoryja@gmail.com>
2019-11-15 01:34:14 +0100

    Add Contructors to Information class
    Forgot to do that, stoopid me

--- Information.java
+++ Information.java
@@ -13,4 +13,32 @@ public class Information
    public boolean loadRooms = false;
    public boolean loadGuests = false;
    public boolean loadStaff = false;
+   
+   // Constructor for saving data
+   public Information( ArrayList<Booking> bookingList,
+                            ArrayList<Booking> archivedBookingList,
+                            ArrayList<Room> roomList,
+                            ArrayList<Guest> guestList,
+                            ArrayList<Staff> staffList )
+   {
+       this.bookingList = bookingList;
+       this.archivedBookingList = archivedBookingList;
+       this.roomList = roomList;
+       this.guestList = guestList;
+       this.staffList =staffList;
+   }
+   
+   // Constructor for loading data from file
+   public Information( boolean loadBookings,
+                           boolean loadArchive,
+                           boolean loadRooms,
+                           boolean loadGuests,
+                           boolean loadStaff )
+   {
+      this.loadBookings = loadBookings;
+      this.loadArchive = loadArchive;
+      this.loadRooms = loadRooms;
+      this.loadGuests = loadGuests;
+      this.loadStaff = loadStaff;
+   }
 }
